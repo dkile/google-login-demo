419ae0de64600a2d6b61a5bd9940577a09c226a5
dkile <dkile532@naver.com>
2023-03-14 23:55:38 +0900

    Set google login

--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -1,11 +1,55 @@
-import Head from 'next/head'
-import Image from 'next/image'
-import { Inter } from 'next/font/google'
-import styles from '@/styles/Home.module.css'
-
-const inter = Inter({ subsets: ['latin'] })
+import Head from "next/head";
+import styles from "@/styles/Home.module.css";
+import Script from "next/script";
+import { useRef } from "react";
 
 export default function Home() {
+  const googleSignInButton = useRef(null);
+
+  const postLoginToken = async (idToken: any) => {
+    const API_URL = "";
+    const path = "/v1/oauth/login";
+
+    try {
+      const res = await fetch(`${API_URL}${path}`, {
+        method: "POST",
+        credentials: "include",
+        headers: {
+          Accept: "application/json",
+          "Content-Type": "application/json",
+        },
+        body: JSON.stringify(idToken),
+      });
+      if (!res.ok) throw new Error("error occurred");
+      console.log(res.json());
+      return true;
+    } catch (err: any) {
+      console.error("Error message:", err.message);
+      return false;
+    }
+  };
+
+  const onSignIn = async (res: any) => {
+    const { credentials } = res;
+    const result = await postLoginToken(credentials);
+    console.log(result);
+  };
+
+  const onLoadGSI = () => {
+    window.google.accounts.id.initialize({
+      client_id:
+        "260952113847-40bmsga5bub0kgp7lnloij2k0qirhacg.apps.googleusercontent.com",
+      callback: onSignIn,
+    });
+
+    window.google.accounts.id.renderButton(googleSignInButton.current, {
+      theme: "filled_blue",
+      size: "large",
+      text: "SignIn",
+      width: "250",
+    });
+  };
+
   return (
     <>
       <Head>
@@ -14,110 +58,10 @@ export default function Home() {
         <meta name="viewport" content="width=device-width, initial-scale=1" />
         <link rel="icon" href="/favicon.ico" />
       </Head>
-      <main className={styles.main}>
-        <div className={styles.description}>
-          <p>
-            Get started by editing&nbsp;
-            <code className={styles.code}>src/pages/index.tsx</code>
-          </p>
-          <div>
-            <a
-              href="https://vercel.com?utm_source=create-next-app&utm_medium=default-template&utm_campaign=create-next-app"
-              target="_blank"
-              rel="noopener noreferrer"
-            >
-              By{' '}
-              <Image
-                src="/vercel.svg"
-                alt="Vercel Logo"
-                className={styles.vercelLogo}
-                width={100}
-                height={24}
-                priority
-              />
-            </a>
-          </div>
-        </div>
-
-        <div className={styles.center}>
-          <Image
-            className={styles.logo}
-            src="/next.svg"
-            alt="Next.js Logo"
-            width={180}
-            height={37}
-            priority
-          />
-          <div className={styles.thirteen}>
-            <Image
-              src="/thirteen.svg"
-              alt="13"
-              width={40}
-              height={31}
-              priority
-            />
-          </div>
-        </div>
-
-        <div className={styles.grid}>
-          <a
-            href="https://nextjs.org/docs?utm_source=create-next-app&utm_medium=default-template&utm_campaign=create-next-app"
-            className={styles.card}
-            target="_blank"
-            rel="noopener noreferrer"
-          >
-            <h2 className={inter.className}>
-              Docs <span>-&gt;</span>
-            </h2>
-            <p className={inter.className}>
-              Find in-depth information about Next.js features and&nbsp;API.
-            </p>
-          </a>
-
-          <a
-            href="https://nextjs.org/learn?utm_source=create-next-app&utm_medium=default-template&utm_campaign=create-next-app"
-            className={styles.card}
-            target="_blank"
-            rel="noopener noreferrer"
-          >
-            <h2 className={inter.className}>
-              Learn <span>-&gt;</span>
-            </h2>
-            <p className={inter.className}>
-              Learn about Next.js in an interactive course with&nbsp;quizzes!
-            </p>
-          </a>
-
-          <a
-            href="https://vercel.com/templates?framework=next.js&utm_source=create-next-app&utm_medium=default-template&utm_campaign=create-next-app"
-            className={styles.card}
-            target="_blank"
-            rel="noopener noreferrer"
-          >
-            <h2 className={inter.className}>
-              Templates <span>-&gt;</span>
-            </h2>
-            <p className={inter.className}>
-              Discover and deploy boilerplate example Next.js&nbsp;projects.
-            </p>
-          </a>
-
-          <a
-            href="https://vercel.com/new?utm_source=create-next-app&utm_medium=default-template&utm_campaign=create-next-app"
-            className={styles.card}
-            target="_blank"
-            rel="noopener noreferrer"
-          >
-            <h2 className={inter.className}>
-              Deploy <span>-&gt;</span>
-            </h2>
-            <p className={inter.className}>
-              Instantly deploy your Next.js site to a shareable URL
-              with&nbsp;Vercel.
-            </p>
-          </a>
-        </div>
+      <Script src="https://accounts.google.com/gsi/client" onLoad={onLoadGSI} />
+      <main>
+        <button ref={googleSignInButton}></button>
       </main>
     </>
-  )
+  );
 }
