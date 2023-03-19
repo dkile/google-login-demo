9ca49f01e36528c8ff42a6526c53748a4492b5e6
dkile <dkile532@naver.com>
2023-03-19 21:21:51 +0900

    make google auth

---
+++ src/components/GoogleLoginButton.tsx
@@ -0,0 +1,77 @@
+import { useState, useEffect } from "react";
+import { useGoogleLogin, googleLogout } from "@react-oauth/google";
+import Image from "next/image";
+import axios from "axios";
+
+type TProfile = {
+  picture: string;
+  name: string;
+  email: string;
+};
+
+export default function GoogleLoginButton() {
+  const [token, setToken] = useState("");
+  const [profile, setProfile] = useState<TProfile | null>(null);
+
+  const login = useGoogleLogin({
+    onSuccess: async (codeResponse) => {
+      console.log(codeResponse);
+      const tokens = await axios.post(
+        "https://api.server.d0lim.com/auth/api/google",
+        {
+          code: codeResponse.code,
+        }
+      );
+
+      console.log(tokens);
+    },
+    onError: (error) => console.log("Login Failed:", error),
+    flow: "auth-code",
+    redirect_uri: "https://api.server.d0lim.com/auth/api/google/callback",
+  });
+
+  useEffect(() => {
+    if (token) {
+      axios
+        .get(
+          `https://www.googleapis.com/oauth2/v1/userinfo?access_token=${token}`,
+          {
+            headers: {
+              Authorization: `Bearer ${token}`,
+              Accept: "application/json",
+            },
+          }
+        )
+        .then((res) => {
+          setProfile(res.data);
+        })
+        .catch((err) => console.log(err));
+    }
+  }, [token]);
+
+  // log out function to log the user out of google and set the profile array to null
+  const logOut = () => {
+    googleLogout();
+    setProfile(null);
+  };
+
+  return (
+    <div>
+      <h2>React Google Login</h2>
+      <br />
+      <br />
+      {profile ? (
+        <div>
+          <h3>User Logged in</h3>
+          <p>Name: {profile.name}</p>
+          <p>Email Address: {profile.email}</p>
+          <br />
+          <br />
+          <button onClick={logOut}>Logout</button>
+        </div>
+      ) : (
+        <button onClick={() => login()}>Sign in with Google 🚀 </button>
+      )}
+    </div>
+  );
+}
--- src/pages/_app.tsx
+++ src/pages/_app.tsx
@@ -1,6 +1,15 @@
-import '@/styles/globals.css'
-import type { AppProps } from 'next/app'
+import "@/styles/globals.css";
+import { GoogleOAuthProvider } from "@react-oauth/google";
+import type { AppProps } from "next/app";
 
 export default function App({ Component, pageProps }: AppProps) {
-  return <Component {...pageProps} />
+  return (
+    <GoogleOAuthProvider
+      clientId={
+        "260952113847-40bmsga5bub0kgp7lnloij2k0qirhacg.apps.googleusercontent.com"
+      }
+    >
+      <Component {...pageProps} />
+    </GoogleOAuthProvider>
+  );
 }
